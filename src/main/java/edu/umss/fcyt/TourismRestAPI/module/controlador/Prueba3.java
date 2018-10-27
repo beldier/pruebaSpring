52ead375d3988aaecdfaaeea4355d4794d6abce2
lurianne <luriannemarianne@gmail.com>
2018-10-27 10:29:48 -0400

    pruebap2

---
+++ src/main/java/edu/umss/fcyt/TourismRestAPI/module/controlador/Prueba3.java
@@ -0,0 +1,9 @@
+package edu.umss.fcyt.TourismRestAPI.module.controlador;
+
+public class Prueba3 {
+    Prueba3  prueba4;
+    public Prueba3()
+    {
+        prueba4 = new Prueba4();
+    }
+}
---
+++ src/main/java/edu/umss/fcyt/TourismRestAPI/module/controlador/Prueba4.java
@@ -0,0 +1,11 @@
+package edu.umss.fcyt.TourismRestAPI.module.controlador;
+
+import javax.print.attribute.standard.PrinterURI;
+
+public class Prueba4 {
+    public Prueba4()
+    {
+        new Prueba3();
+    }
+
+}
